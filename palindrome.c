82ed2ed2701a5a16395f1052a76cefa56a6136b9
nikoladragomirovic <132782535+nikoladragomirovic@users.noreply.github.com>
2023-10-19 16:16:45 +0200

    Updated to follow MISRA C

--- palindrome.c
+++ palindrome.c
@@ -5,8 +5,8 @@
 #define DECIMAL 10
 #define HEXADECIMAL 16
 
-static void print_palindromes(const int32_t, const int32_t);
-static int8_t is_palindrome(const int32_t, const const int32_t);
+static void print_palindromes(const int32_t target, const int32_t base);
+static int8_t is_palindrome(const int32_t num, const int32_t base);
 
 static int8_t is_palindrome(const int32_t num, const int32_t base)
 {
@@ -17,39 +17,63 @@ static int8_t is_palindrome(const int32_t num, const int32_t base)
     while (temp > 0)
     {
         int32_t digit = temp % base;
-        reversed_num = reversed_num * base + digit;
+        reversed_num = (reversed_num * base) + digit;
         temp /= base;
     }
 
-    return num == reversed_num ? 1 : 0;
+    return (int8_t)(num == reversed_num ? 1 : 0);
 }
 
 static void print_palindromes(const int32_t target, const int32_t base)
 {
     if (target <= 0)
     {
-        printf("Target number must be greater than 0.\n");
-        return; 
-    }
-
-    const char *format_string = (base == DECIMAL) ? "%" PRId32 "\n" :
-                                (base == HEXADECIMAL) ? "%" PRIX32 "\n" :
-                                (base == OCTAL) ? "%" PRIo32 "\n" : "\n";
-
-    for (int32_t i = 0; i < target; i++)
-    {
-        if (is_palindrome(i, base))
+        for (int32_t i = 0; i < target; i++)
         {
-            printf(format_string, i);
+            if (is_palindrome(i, base))
+            {
+                switch (base)
+                {
+                case OCTAL:
+                    printf("%" PRIo32 "\n", i);
+                    break;
+                case DECIMAL:
+                    printf("%" PRId32 "\n", i);
+                    break;
+                case HEXADECIMAL:
+                    printf("%" PRIX32 "\n", i);
+                    break;
+                default:
+                    break;
+                }
+            }
         }
+    }
+    else
+    {
+        printf("Target number must be greater than 0.\n");
     }
 }
 
 int main()
 {
-    int32_t target = 200000;
+    int32_t target, base;
 
-    print_palindromes(target, OCTAL);
+    while (1)
+    {
+        printf("Enter a target number (or -1 to exit): ");
+        scanf("%" SCNd32, &target);
+
+        if (target == -1)
+        {
+            break;
+        }
+
+        printf("Enter the base (8 for octal, 10 for decimal, 16 for hexadecimal): ");
+        scanf("%" SCNd32, &base);
+
+        print_palindromes(target, base);
+    }
 
     return 0;
 }
